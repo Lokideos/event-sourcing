10a5630a565d104860dd09b248f068fe6100e664
lokideos <archmage09@gmail.com>
2021-08-16 23:15:49 +0300

    Update main flow for producers usage

--- lib/main.rb
+++ lib/main.rb
@@ -1,46 +1,28 @@
 # frozen_string_literal: true
 
+require 'securerandom'
 require_relative './events'
 require_relative './event_store'
 require_relative './projections'
+require_relative './producers'
 
 class Main; end
 
-puts 'Initial state:'
 event_store = EventStore.new
 project = Projections::Project.new
 
-puts '*' * 50
-
-puts 'After creating order:'
-event = Events::OrderCreated.new(payload: { order_id: 1, account_id: 1 })
-event_store.append(event)
-
-p events = event_store.get
+events = event_store.get
 pp project.call(Projections::AllOrders.new, {}, events)
 
-puts '*' * 50
+puts '*' * 80
 
-puts 'After creating one more order:'
-event = Events::OrderCreated.new(payload: { order_id: 2, account_id: 1 })
+event = Events::OrderCreated.new(payload: { order_id: SecureRandom.uuid, account_id: 1 })
 event_store.append(event)
+
+event_store.evolve(Producers::AddItem.new, account_id: 1, name: 'ruby sticker', cost: 10)
+
+event_store.evolve(Producers::AddItem.new, account_id: 2, name: 'hanami sticker', cost: 5)
+event_store.evolve(Producers::AddItem.new, account_id: 2, name: 'ruby sticker', cost: 15)
+
+events = event_store.get
 pp project.call(Projections::AllOrders.new, {}, events)
-
-puts '*' * 50
-
-puts 'After adding items to orders:'
-event = Events::ItemAddedToOrder.new(payload: { order_id: 1, item_id: 1, name: 'ruby sticker', cost: 10 })
-event_store.append(event)
-
-event = Events::ItemAddedToOrder.new(payload: { order_id: 1, item_id: 2, name: 'git sticker', cost: 17 })
-event_store.append(event)
-
-event = Events::ItemAddedToOrder.new(payload: { order_id: 2, item_id: 3, name: 'ruby sticker', cost: 11 })
-event_store.append(event)
-
-pp project.call(Projections::AllOrders.new, {}, events)
-
-puts '*' * 50
-
-puts 'Calculation cost for orders:'
-pp project.call(Projections::CostForOrders.new, {}, events)
